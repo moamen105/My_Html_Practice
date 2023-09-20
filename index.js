d396f6953d782ece0d01b66b03e95d419f64f8f7
moamen105 <almonshake75@gmail.com>
2023-09-20 15:01:03 +0300

    while loop

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 var breaks = [];
 var counter = 1;
+var number = 1;
+
 
 function pomodoro()
 {
-    if (counter === 4){
+    if (counter === 4)
+    {
         breaks.push(15);
         counter = 1;
     }
-    
+            
     else
     {
         breaks.push(5);
         counter++;
     }
 
-    
-
-    console.log(breaks);
 
 
 }
+while(number <= 9){
+    pomodoro();
+    number++;
+}
+console.log(breaks);
 
