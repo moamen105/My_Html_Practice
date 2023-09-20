79b6d977195f9dc0910720a5e910ddfae7dc43b6
moamen105 <almonshake75@gmail.com>
2023-09-20 12:47:47 +0300

    random (ceil for up and floor for down numbers).

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function becomeMember()
 {
     console.log("Wlcome you are a member Now!");
 }
+
+var randomNum1 = Math.ceil(1.9);
+var randomNum2 = Math.floor(1.9);
+
+console.log("your random number is : " + randomNum1);
+console.log("your random number is : " + randomNum2);
