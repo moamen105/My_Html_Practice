be2fd69fda97c473badc163ed80913d9f3847651
moamen105 <almonshake75@gmail.com>
2023-09-29 18:28:28 +0300

    used classes and toggle instead of style.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function review(x) {
     if(x === 0){
-        document.querySelectorAll(".review")[0].style.opacity = '100%';
-        document.querySelectorAll(".productimg")[0].style.filter = 'brightness(0.3)';
+        document.querySelectorAll(".review")[0].classList.toggle("hover-review-js");
+        document.querySelectorAll(".productimg")[0].classList.toggle("hover-img-js");
     }
     else if (x === 1){
-        document.querySelectorAll(".review")[1].style.opacity = '100%';
-        document.querySelectorAll(".productimg")[1].style.filter = 'brightness(0.3)';
+        document.querySelectorAll(".review")[1].classList.toggle("hover-review-js");
+        document.querySelectorAll(".productimg")[1].classList.toggle("hover-img-js");
 
     }
     else if (x === 2){
-        document.querySelectorAll(".review")[2].style.opacity = '100%';
-        document.querySelectorAll(".productimg")[2].style.filter = 'brightness(0.3)';
+        document.querySelectorAll(".review")[2].classList.toggle("hover-review-js");
+        document.querySelectorAll(".productimg")[2].classList.toggle("hover-img-js");
 
     }
     else if (x === 3){
-        document.querySelectorAll(".review")[3].style.opacity = '100%'; 
-        document.querySelectorAll(".productimg")[3].style.filter = 'brightness(0.3)';
+        document.querySelectorAll(".review")[3].classList.toggle("hover-review-js");
+        document.querySelectorAll(".productimg")[3].classList.toggle("hover-img-js");
 
     }
 
@@ -24,22 +24,22 @@ function review(x) {
 
 function unreview(x) {
     if(x === 0){
-        document.querySelectorAll(".review")[0].style.opacity = '0%';
-        document.querySelectorAll(".productimg")[0].style.filter = 'brightness(1)';
+        document.querySelectorAll(".review")[0].classList.toggle("hover-review-js");
+        document.querySelectorAll(".productimg")[0].classList.toggle("hover-img-js");
     }
     else if (x === 1){
-        document.querySelectorAll(".review")[1].style.opacity = '0%';
-        document.querySelectorAll(".productimg")[1].style.filter = 'brightness(1)';
+        document.querySelectorAll(".review")[1].classList.toggle("hover-review-js");
+        document.querySelectorAll(".productimg")[1].classList.toggle("hover-img-js");
 
     }
     else if (x === 2){
-        document.querySelectorAll(".review")[2].style.opacity = '0%';
-        document.querySelectorAll(".productimg")[2].style.filter = 'brightness(1)';
+        document.querySelectorAll(".review")[2].classList.toggle("hover-review-js");
+        document.querySelectorAll(".productimg")[2].classList.toggle("hover-img-js");
 
     }
     else if (x === 3){
-        document.querySelectorAll(".review")[3].style.opacity = '0%'; 
-        document.querySelectorAll(".productimg")[3].style.filter = 'brightness(1)';
+        document.querySelectorAll(".review")[3].classList.toggle("hover-review-js");
+        document.querySelectorAll(".productimg")[3].classList.toggle("hover-img-js");
 
     }
 }
