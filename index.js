9d0fc312fe0dac7d2d723632ede8e94a1c493d86
moamen105 <almonshake75@gmail.com>
2023-09-29 17:06:43 +0300

    review effect by javaScript

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-function review() {
-    document.querySelector(".review").style.opacity = '100%';
-    document.querySelector("#productimg").style.filter = "brightness(0.4)"
+function review(x) {
+    if(x === 0){
+        document.querySelectorAll(".review")[0].style.opacity = '100%';
+        document.querySelectorAll(".productimg")[0].style.filter = 'brightness(0.3)';
+    }
+    else if (x === 1){
+        document.querySelectorAll(".review")[1].style.opacity = '100%';
+        document.querySelectorAll(".productimg")[1].style.filter = 'brightness(0.3)';
+
+    }
+    else if (x === 2){
+        document.querySelectorAll(".review")[2].style.opacity = '100%';
+        document.querySelectorAll(".productimg")[2].style.filter = 'brightness(0.3)';
+
+    }
+    else if (x === 3){
+        document.querySelectorAll(".review")[3].style.opacity = '100%'; 
+        document.querySelectorAll(".productimg")[3].style.filter = 'brightness(0.3)';
+
+    }
+
+
 }
 
-function unreview() {
-    document.querySelector(".review").style.opacity = '0%';
-    document.querySelector("#productimg").style.brightness = '100%';
+function unreview(x) {
+    if(x === 0){
+        document.querySelectorAll(".review")[0].style.opacity = '0%';
+        document.querySelectorAll(".productimg")[0].style.filter = 'brightness(1)';
+    }
+    else if (x === 1){
+        document.querySelectorAll(".review")[1].style.opacity = '0%';
+        document.querySelectorAll(".productimg")[1].style.filter = 'brightness(1)';
+
+    }
+    else if (x === 2){
+        document.querySelectorAll(".review")[2].style.opacity = '0%';
+        document.querySelectorAll(".productimg")[2].style.filter = 'brightness(1)';
+
+    }
+    else if (x === 3){
+        document.querySelectorAll(".review")[3].style.opacity = '0%'; 
+        document.querySelectorAll(".productimg")[3].style.filter = 'brightness(1)';
+
+    }
 }
