bdc3040db2707d9985c70ca48ff679dbc19e9a05
moamen105 <almonshake75@gmail.com>
2023-10-04 18:07:18 +0300

    .

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//-------------------------- Review hover on product ----------------------------
+//-------------------------------------------------------------------------------
+
 function review(x) {
     if(x === 0){
         document.querySelectorAll(".review")[0].classList.toggle("hover-review-js");
@@ -18,8 +21,6 @@ function review(x) {
         document.querySelectorAll(".productimg")[3].classList.toggle("hover-img-js");
 
     }
-
-
 }
 
 function unreview(x) {
@@ -44,6 +45,18 @@ function unreview(x) {
     }
 }
 
+document.getElementsByClassName("Pro_Style")[0].addEventListener("mouseover",function(){review(0)})
+document.getElementsByClassName("Pro_Style")[0].addEventListener("mouseout",function(){unreview(0)})
+document.getElementsByClassName("Pro_Style")[1].addEventListener("mouseover",function(){review(1)})
+document.getElementsByClassName("Pro_Style")[1].addEventListener("mouseout",function(){unreview(1)})
+document.getElementsByClassName("Pro_Style")[2].addEventListener("mouseover",function(){review(2)})
+document.getElementsByClassName("Pro_Style")[2].addEventListener("mouseout",function(){unreview(2)})
+document.getElementsByClassName("Pro_Style")[3].addEventListener("mouseover",function(){review(3)})
+document.getElementsByClassName("Pro_Style")[3].addEventListener("mouseout",function(){unreview(3)})
+
+
+//-------------------------------- dark Mode ------------------------------------
+//-------------------------------------------------------------------------------
 
 sun = document.getElementById("sun")
 moon = document.getElementById("moon")
@@ -72,6 +85,9 @@ function lightMode()
     product.classList.toggle("darkMode-product")
 }
 
+//-------------------------------- calculator -----------------------------------
+//-------------------------------------------------------------------------------
+
 var number1 = "";
 var number2 = "";
 var isNumber1IsReady = false;
@@ -81,4 +97,13 @@ function formNum(n){
         number1 += n;
         document.getElementById("displayNum").textContent = number1;
     }
+    else{
+        number2 += n;
+        document.getElementById("displayNum").textContent = number2;
+    }
 }
+
+function sum(n1,n2){       return n1+n2}
+function multiply(n1,n2){    return n1*n2}
+function subtract(n1,n2){    return n1-n2}
+function devide(n1,n2){    return n1/n2}
